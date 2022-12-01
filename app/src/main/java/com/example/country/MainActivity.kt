fb6da553c6c96de0ed17fccb493f8abfc78a3866
Witiania <Witiania@gmail.com>
2022-12-01 21:06:33 +0300

    Create coroutines

--- app/src/main/java/com/example/country/MainActivity.kt
+++ app/src/main/java/com/example/country/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.country
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.example.country.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -10,5 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSearch.setOnClickListener {
+            val countryName = binding.etCountryName.text.toString() //greece
+
+            lifecycleScope.launch{ // Так как ранее пометили функцию getCountrieByName как корутину, необходимо создать саму корутину
+            val countries = restCountriesAPI.getCountrieByName(countryName) // Код будет выполнятся в фоном потоке параллельно с основным
+            val country:Country=countries[0]
+
+            binding.tvCountryName.text = country.name
+            binding.tvCapital.text = country.capital
+            binding.tvArea.text = country.area.toString()
+            binding.tvPopulation.text = country.population.toString()
+            binding.tvLanguages.text = country.languages[0].toString()
+            binding.tvNameCurrencies.text = country.currencies[0].toString()
+            binding.tvSymbolCurrencies.text = country.currencies[1].toString()}
+
+
+        }
     }
 }
--- app/src/main/java/com/example/country/RemoteDataSource.kt
+++ app/src/main/java/com/example/country/RemoteDataSource.kt
@@ -1,5 +1,6 @@
 package com.example.country
 
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -8,10 +9,10 @@ import retrofit2.http.Path
 
 interface RestCountriesAPI {
     @GET("name/{name}")
-    fun getCountrieByName(@Path("name") cityName: String): List<Country>
+   suspend fun getCountrieByName(@Path("name") cityName: String): List<Country> // помечаем корутину, эта функция может работать долго...
 }
 
-var retrofit = Retrofit.Builder()
+var retrofit: Retrofit = Retrofit.Builder()
     .baseUrl("https://countriesinfo21.herokuapp.com/")
     .addConverterFactory(GsonConverterFactory.create())
     .build()
